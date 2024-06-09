b467ecab8f0e5a7ecfa976da0a89b2e6d22f4891
StealthSec <166227969+Stealth-Sec@users.noreply.github.com>
2024-06-09 13:53:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("Test 1 Sucessfull")
 import time
 time.sleep(10)
 print("Test 2 Sucessfull")
+import subprocess
+subprocess.run(["pip", "install", "selenium"])
+if subprocess.returncode == 0:
+    print("Test 3 Sucessfull")
+else:
+    print(f"Failed to install Selenium using pip. Return code: {subprocess.returncode}")
